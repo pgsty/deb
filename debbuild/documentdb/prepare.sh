0aa4af71b1a7b239195bff7cfceb181711d872e5
Feng Ruohang <rh@vonng.com>
2026-06-30 18:18:55 +0800

    bump documentdb to 0.113

--- debbuild/documentdb/prepare.sh
+++ debbuild/documentdb/prepare.sh
@@ -1,7 +1,7 @@
 # get documentdb source tarball
 # pig build get documentdb
 
-TARBALL=${1-'documentdb-0.112-0.tar.gz'}
+TARBALL=${1-'documentdb-0.113-0.tar.gz'}
 SOURCE=${SOURCE:-}
 
 if [ -z "${SOURCE}" ]; then
