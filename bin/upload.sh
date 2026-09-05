b8b4bade96f8c889da2fbafb12be5a00e01f6ce4
Feng Ruohang <rh@vonng.com>
2026-09-05 08:42:09 +0800

    exclude build artifacts from recipe uploads

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
+set -euo pipefail
 
 # script to create debbuild.tar.gz and upload to pigsty.cc / pigsty.io
 
-PROG_NAME="$(basename $0)"
-BIN_DIR="$(cd $(dirname $0) && pwd)"
-HOME_DIR="$(cd ${BIN_DIR}/.. && pwd)"
+BIN_DIR="$(cd "$(dirname "$0")" && pwd)"
+HOME_DIR="$(cd "${BIN_DIR}/.." && pwd)"
 TMP_DIR="$HOME_DIR/tmp"
-DEBBUILD_DIR="$HOME_DIR/debbuild"
 TARBALL_NAME="debbuild.tar.gz"
 
 # make tarball
-echo "build tmp/debbuild"
-rm -rf "${TMP_DIR}/debbuild"
-cp -r ${DEBBUILD_DIR} ${TMP_DIR}
+echo "build tmp/${TARBALL_NAME}"
+mkdir -p "${TMP_DIR}"
+COPYFILE_DISABLE=1 gtar -zcf "${TMP_DIR}/${TARBALL_NAME}" \
+    --exclude-vcs --exclude='.DS_Store' --exclude='._*' \
+    --exclude='debbuild/SOURCES' --exclude='debbuild/*/build' \
+    --exclude='*.deb' --exclude='*.ddeb' --exclude='*.changes' \
+    --exclude='*.buildinfo' --exclude='*.log' \
+    -C "${HOME_DIR}" debbuild
 cd "${TMP_DIR}"
-gtar -zcf debbuild.tar.gz debbuild
-RPM_TARBALL="${TMP_DIR}/${TARBALL_NAME}"
 
 # print info
 ls -alh "${TARBALL_NAME}"
 md5sum  "${TARBALL_NAME}"
 
 # upload to cloud
-cp "${TARBALL_NAME}" ~/pgsty/repo/ext/spec/${TARBALL_NAME}
+cp "${TARBALL_NAME}" "$HOME/pgsty/repo/ext/spec/${TARBALL_NAME}"
 rclone copyto "${TARBALL_NAME}" "cos:/repo-1304744452/ext/spec/${TARBALL_NAME}"
 rclone copyto "${TARBALL_NAME}" "cf:/repo/ext/spec/${TARBALL_NAME}"
 
